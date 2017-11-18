c27232cbd280acc79124bb75f2e0d178030a0757
Lohit <lohit8846@gmail.com>
2017-11-17 19:12:51 -0700

    http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -12,7 +13,8 @@ import { AngularFontAwesomeModule } from 'angular-font-awesome/angular-font-awes
   ],
   imports: [
     BrowserModule,
-    AngularFontAwesomeModule
+    AngularFontAwesomeModule,
+    HttpModule
 
   ],
   providers: [],
